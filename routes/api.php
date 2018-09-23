f8d6c9908d8a6cc9ee873de778c1b79b50acba2b
Megan Ayucedar <mgncdr@gmail.com>
2018-09-24 02:30:56 +0700

    ...

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource('giveaways', 'Api\GiveawayController', [ 'as' => 'api' ]);
-Route::apiResource('giveaways.participants', 'Api\GiveawayParticipantController', [ 'as' => 'api' ]);
-//Route::post('giveaways/{giveaway_id}/participants', 'Api\GiveawayController@store')->name('api.giveaways.store');
-//Route::delete('giveaways/{giveaway_id}/participants/{participant}', 'Api\GiveawayController@delete')->name('api.giveaways.destroy');
+//Route::apiResource('giveaways.participants', 'Api\GiveawayParticipantController', [ 'as' => 'api' ]);
+Route::post('giveaways/{giveaway_id}/participants', 'Api\GiveawayController@store')->name('api.giveaways.store');
+Route::delete('giveaways/{giveaway_id}/participants/{participant}', 'Api\GiveawayController@delete')->name('api.giveaways.destroy');
+Route::put('giveaways/{giveaway_id}/participants/{participant}', 'Api\GiveawayController@update')->name('api.giveaways.update');
 Route::get('giveaways/show/{user_id}', 'Api\GiveawayController@showByUser')->name('api.giveaways.showbyuser');
 Route::get('giveaways/{user_id}/joined', 'Api\GiveawayController@showJoinedGiveaway')->name('api.giveaways.joined');
 Route::get('giveaways/{user_id}/won', 'Api\GiveawayController@giveawayWon')->name('api.giveaways.won');
 Route::get('giveaways/{giveaway_id}/participants/winner', 'Api\GiveawayParticipantController@getWinner')->name('api.giveaways.participants.winner');
-Route::put('giveaways/{giveaway_id}/participants/{user_id}', 'Api\GiveawayParticipantController@setWinner')->name('api.giveaways.participants.winner');
+//Route::put('giveaways/{giveaway_id}/participants/{user_id}', 'Api\GiveawayParticipantController@setWinner')->name('api.giveaways.participants.winner');
